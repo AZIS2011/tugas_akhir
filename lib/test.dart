377c60b54f055f1a6639b624ba63e5588b5d9692
Fahrul Riza <rizafahrul16@gmail.com>
2023-07-28 15:50:32 +0700

    Second Commit

--- lib/test.dart
+++ lib/test.dart
@@ -1,44 +1,44 @@
-// import 'dart:convert';
-// import 'dart:core';
+import 'dart:convert';
+import 'dart:core';
 
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-// class Testing extends StatefulWidget {
-//   const Testing({Key? key}) : super(key: key);
+class Testing extends StatefulWidget {
+  const Testing({Key? key}) : super(key: key);
 
-//   @override
-//   State<Testing> createState() => _TestingState();
-// }
+  @override
+  State<Testing> createState() => _TestingState();
+}
 
-// class _TestingState extends State<Testing> {
-//   final String url = 'http://127.0.0.1:8000/api/products';
+class _TestingState extends State<Testing> {
+  final String url = 'http://127.0.0.1:8000/api/products';
 
-//   Future getProducts() async {
-//     var response = await http.get(Uri.parse(url));
-//     print(json.decode(response.body));
-//     return json.decode(response.body);
-//   }
+  Future getProducts() async {
+    var response = await http.get(Uri.parse(url));
+    print(json.decode(response.body));
+    return json.decode(response.body);
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('TESTING'),
-//       ),
-//       body: FutureBuilder<dynamic>(
-//           future: getProducts(),
-//           builder: (context, snapshot) {
-//             if (snapshot.hasData) {
-//               return ListView.builder(
-//                   itemCount: snapshot.data['data'].length,
-//                   itemBuilder: (context, index) {
-//                     return Text(snapshot.data['data'][index]['name']);
-//                   });
-//             } else {
-//               return Text('data error');
-//             }
-//           }),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TESTING'),
+      ),
+      body: FutureBuilder<dynamic>(
+          future: getProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data['data'].length,
+                  itemBuilder: (context, index) {
+                    return Text(snapshot.data['data'][index]['name']);
+                  });
+            } else {
+              return Text('data error');
+            }
+          }),
+    );
+  }
+}
